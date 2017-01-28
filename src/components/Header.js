7c9506993d5cbc0c5470513dbc1c0b9be2bdb7c5
Markus Laurila <markuslaurila@outlook.com>
2017-01-28 04:48:52 +0200

    clicking logo goes to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,7 @@ import { Link } from 'react-router';
 function Header () {
     return (
       <header className='header'>
-        <div className='logo-container'>
-        </div>
+        <Link to='/'><div className='logo-container'></div></Link>
         <nav className='navigation'>
           <Link className='navigation__link' activeClassName="navigation__link_active" to='/categories'>Categories</Link>
           <Link className='navigation__link' activeClassName="navigation__link_active" to='/add'>Add&nbsp;questions</Link>
