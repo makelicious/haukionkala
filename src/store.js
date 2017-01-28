72fea1f2723d08ce8edfec8e884c23d9a35bfd03
Markus Laurila <markuslaurila@outlook.com>
2017-01-28 02:10:14 +0200

    set up localstorage
    cards need to be saved

--- src/store.js
+++ src/store.js
@@ -2,6 +2,11 @@ import qaApp from './reducers/index';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const store = applyMiddleware(thunk)(createStore)(qaApp);
+const localSave = localStorage.getItem('app') !== null ? JSON.parse(localStorage.getItem('app')) : {};
+const store = createStore(qaApp, localSave, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  localStorage.setItem('app', JSON.stringify(store.getState()));
+})
 
 export default store;
