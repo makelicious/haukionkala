9786c89b3cf59b3715311512c3f03148744a4f53
Markus Laurila <markuslaurila@outlook.com>
2017-01-31 00:14:33 +0200

    fix link, change styles and intro

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 function Header() {
   return (
     <header className='header'>
-      <Link to='/main'><div className='logo-container' /></Link>
+      <Link to='/'><div className='logo-container' /></Link>
       <nav className='navigation'>
         <Link className='navigation__link' activeClassName="navigation__link_active" to='/categories'>Categories</Link>
         <Link className='navigation__link' activeClassName="navigation__link_active" to='/add'>Add&nbsp;questions</Link>
--- src/components/frontpage.js
+++ src/components/frontpage.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 function frontPage() {
   return (
     <div className='front-page'>
     <div className='front-page__header'>
       <div className='logo-container'></div>
-      <h1>= KALA </h1>
+      <h1>&nbsp;= KALA </h1>
     </div>
       <div className='front-page__content'>
         <h2>Have you ever tried to remember something?</h2>
         <p>Look no further!</p>
-        <p> Hauki=kala is an app which provides you a way to remember stuff, with ease</p>
-        <a href='/study'>Jump right in!</a>
+        <p> Hauki = kala is an app that provides you a quick way to remember stuff</p>
+        <Link to='/study' className='front-page__link'>Jump right in!</Link>
       </div>
     </div>
   )
