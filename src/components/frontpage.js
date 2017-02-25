d6332f398d4b8b2bedd3415a0d27b5c3ea7b235c
Markus Laurila <markuslaurila@outlook.com>
2017-02-25 13:49:59 +0200

    make frontpage little more eye-friendly

--- src/components/frontpage.js
+++ src/components/frontpage.js
@@ -9,9 +9,9 @@ function frontPage() {
       <h1>&nbsp;= KALA</h1>
     </div>
       <div className='front-page__content'>
-        <h2>Have you ever tried to remember something?</h2>
-        <p>Look no further!</p>
-        <p> Hauki = kala is an app that provides you a quick way to remember stuff</p>
+        <h2 className='front-page__content__title'>Have you ever tried to remember something?</h2>
+        <p> Hauki = kala provides you a quick way to remember stuff using flashcards</p>
+        <p>Add your own questions and answers on a subject you want to learn and start flashing</p>
         <Link to='/study' className='front-page__link'>Jump in!</Link>
       </div>
     </div>
